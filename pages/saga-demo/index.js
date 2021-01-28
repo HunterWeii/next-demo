9788b8c6538aceba9735f5a53f1092ec2ade8171
tanhw <tanhw@tphq.local>
2021-01-28 12:35:36 +0800

    test webhooks

--- pages/saga-demo/index.js
+++ pages/saga-demo/index.js
@@ -2,6 +2,8 @@ import { Provider } from "react-redux";
 import { store } from "stores/saga-demo";
 import Counter from "components/pages/saga-demo/Counter";
 
+console.log("test web hook")
+
 export default function SagaDemo() {
   return (
     <Provider store={ store }>
